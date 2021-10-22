374a137e2caae9693c0737aed146ac0c9e75c374
PullDakar <mihailfufaev@mail.ru>
2021-10-22 18:47:34 +0300

    initial commit

---
+++ src/main/java/ru/jat/trafficcontrol/TrafficControlApplication.java
@@ -0,0 +1,13 @@
+package ru.jat.trafficcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafficControlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrafficControlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/jat/trafficcontrol/TrafficControlApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.jat.trafficcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrafficControlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
